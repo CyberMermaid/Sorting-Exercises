71eedcba2dbc8c42947c4cb12f848c29dce3d975
CyberMermaid <45833464+CyberMermaid@users.noreply.github.com>
2024-05-19 19:56:28 -0400

    Fixed with recursion

--- bubble.js
+++ bubble.js
@@ -1,19 +1,22 @@
 function bubbleSort(arr) {
    let swapped = false
     while(!swapped) {
-      for(let i = 0; i < arr.length; i++) {
+      for(let i = 0; i < arr.length-1; i++) {
         //Compare current element with next one
-        if(arr[i] < arr[i - 1]) {
+        if(arr[i] > arr[i + 1]) {
         // Swap elements if they are in wrong order
           let temp = arr[i];
-          arr[i] = arr[i - 1];
-          arr[i - 1] = temp;
+          arr[i] = arr[i + 1];
+          arr[i + 1] = temp;
           // Set swapped to true if any swap occurred
           swapped = true;
         }
-      }
+    }
+    if (swapped){
+        bubbleSort(arr);
+        swapped = false;
     }
     return arr;
-}
+}}
 
 module.exports = bubbleSort;
